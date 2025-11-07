2b506aefb3c7a18c7b13d176547cd2139c9d88a2
aldys002 <igidiaaldys@gmail.com>
2025-11-07 12:41:57 +0700

    menambahkan TopAppBar pada halaman ListPeserta

--- app/src/main/java/com/example/navigasi/view/ListPeserta.kt
+++ app/src/main/java/com/example/navigasi/view/ListPeserta.kt
@@ -3,9 +3,14 @@ package com.example.navigasi.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.navigasi.R
@@ -21,4 +26,19 @@ fun ListPeserta(
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Perempuan"),
         Pair(first = stringResource(id = R.string.alamat), second = "Sleman, Yogyakarta")
     )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = listOf(R.string.list_peserta),
+                        color = Color.White)
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(color = 0xFF00897B)
+                )
+            )
+        }
+    ) {  }
 }
