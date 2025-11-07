66d980f0064071775400968fd5304902388a7184
aldys002 <igidiaaldys@gmail.com>
2025-11-07 11:57:17 +0700

    menambah fungsi DataApp dan navigasi awal ke halaman Beranda

--- app/src/main/java/com/example/navigasi/DataApp.kt
+++ app/src/main/java/com/example/navigasi/DataApp.kt
@@ -1,7 +1,39 @@
 package com.example.navigasi
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Beranda,
     ListPeserta,
     FormPendaftaran
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Beranda.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = Navigasi.Beranda.name) {
+                Beranda(
+                    onSubmitClick = {
+                        navController.navigate(Navigasi.ListPeserta.name)
+                    }
+                )
+            }
+        }
+    }
 }
