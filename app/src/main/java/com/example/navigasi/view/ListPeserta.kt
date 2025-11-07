a7a20767c208d789121444eafc2e9a3d50ee4935
aldys002 <igidiaaldys@gmail.com>
2025-11-07 12:34:39 +0700

    menguah codingan pertama tadi dengan menambah data dummy peserta pada composable ListPeserta

--- app/src/main/java/com/example/navigasi/view/ListPeserta.kt
+++ app/src/main/java/com/example/navigasi/view/ListPeserta.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import com.example.navigasi.R
 
 
@@ -15,10 +16,9 @@ fun ListPeserta(
     onBerandaClick : () ->,
     onDaftarClick : () ->
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(dimensionResource(id = R.dimen.padding_medium)),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {  }
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Aldys Igidia Triatmaja"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Perempuan"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Sleman, Yogyakarta")
+    )
 }
