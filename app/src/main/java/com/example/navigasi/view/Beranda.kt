8aece626ef815654ba28aa78bda37ca621954f45
aldys002 <igidiaaldys@gmail.com>
2025-11-07 12:14:34 +0700

    membuat tampilan Beranda dengan teks selamat datang dan gambar ilustrasi

--- app/src/main/java/com/example/navigasi/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi/view/Beranda.kt
@@ -1,13 +1,22 @@
 package com.example.navigasi.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.navigasi.R
 
 @Composable
@@ -20,5 +29,14 @@ fun Beranda (
             .padding(dimensionResource(id = R.dimen.padding_medium)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {  }
+    ) {
+        Text(text = stringResource(id = R.string.selamat_datang))
+        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+        Image(
+            painter = painterResource(id = ),
+            contentDescription = null,
+            modifier = Modifier.size(size = 120.dp),
+            contentScale = ContentScale.Fit
+        )
+    }
 }
