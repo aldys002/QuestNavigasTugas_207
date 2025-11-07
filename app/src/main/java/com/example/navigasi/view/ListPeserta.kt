7c291c5f14f53746976d357d5b0a00217b4d820d
aldys002 <igidiaaldys@gmail.com>
2025-11-07 12:57:20 +0700

    menambahkan garis pemisah

--- app/src/main/java/com/example/navigasi/view/ListPeserta.kt
+++ app/src/main/java/com/example/navigasi/view/ListPeserta.kt
@@ -1,9 +1,13 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigasi.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -69,6 +73,10 @@ fun ListPeserta(
                             fontSize = 22.sp
                         )
                     }
+                    HorizontalDivider(
+                        thickness = dimensionResource(id = R.dimen.thickness_divider)
+                        color = Color.Cyan
+                    )
                 }
             }
         }
