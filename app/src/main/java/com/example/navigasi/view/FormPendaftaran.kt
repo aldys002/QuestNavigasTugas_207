77064db6713d77ab7f461f03e418e38017fed770
aldys002 <igidiaaldys@gmail.com>
2025-11-07 13:34:17 +0700

    menambahkan Scaffold dan TopAppBar pada halaman FormPendaftaran

--- app/src/main/java/com/example/navigasi/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/navigasi/view/FormPendaftaran.kt
@@ -2,9 +2,16 @@ package com.example.navigasi.view
 
 import androidx.annotation.OptIn
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.navigasi.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -16,3 +23,19 @@ fun FormPendaftaran(
     var textJK by remember { mutableStateOf("") }
 
     val gender = listOf("Laki-laki", "Perempuan")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.form_pendaftaran),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(color = 0xFF7E57C2)
+                )
+            )
+        }
+    ) {  }
