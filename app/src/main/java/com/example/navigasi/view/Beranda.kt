bd14f6f99e47872a151adb4c22eb59304402283a
aldys002 <igidiaaldys@gmail.com>
2025-11-07 12:11:05 +0700

    membuat composable Beranda dengan tombol navigasi onSubmitClick

---
+++ app/src/main/java/com/example/navigasi/view/Beranda.kt
@@ -0,0 +1,24 @@
+package com.example.navigasi.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.navigasi.R
+
+@Composable
+fun Beranda (
+    onSubmitClick : () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {  }
+}
