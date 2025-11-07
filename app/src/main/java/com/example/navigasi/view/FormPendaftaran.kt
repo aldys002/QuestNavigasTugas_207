fb9c66ee3417b0ba3531fa747276f7583e6f615d
aldys002 <igidiaaldys@gmail.com>
2025-11-07 13:48:23 +0700

    menambahkan field input Nama Lengkap di FormPendaftaran

--- app/src/main/java/com/example/navigasi/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/navigasi/view/FormPendaftaran.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -24,7 +25,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigasi.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -77,7 +80,20 @@ fun FormPendaftaran(
                     verticalArrangement = Arrangement.spacedBy(
                         dimensionResource(id = R.dimen.padding_small)
                     )
-                ) {  }
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.nama_lengkap).uppercase(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp
+                    )
+
+                    OutlinedTextField(
+                        value = textNama,
+                        onValueChange = {textNama = it},
+                        label = {Text(stringResource(id = R.string.nama_lengkap))},
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
 
             }
