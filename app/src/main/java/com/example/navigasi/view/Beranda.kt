bb4ebaa12377caabf56c59011abf5bb00fbf99b5
aldys002 <igidiaaldys@gmail.com>
2025-11-07 12:22:51 +0700

    menambahkan teks nama dan NIM pengguna serta tombol navigasi di Beranda

--- app/src/main/java/com/example/navigasi/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi/view/Beranda.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -38,5 +39,12 @@ fun Beranda (
             modifier = Modifier.size(size = 120.dp),
             contentScale = ContentScale.Fit
         )
+        Spacer(modifier = Modifier.height(height = 20.dp))
+        Text(text = stringResource(id = R.string.nama_pengguna))
+        Text(text = stringResource(id = R.string.nim_pengguna))
+        Spacer(modifier = Modifier.height(height = 30.dp))
+        Button(onClick = onSubmitClick) {
+            Text(text = stringResource(id = R.string.submit))
+        }
     }
 }
